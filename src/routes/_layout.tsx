6484dd60e10ee8ec96f55a4fb1ef72bd899c1019
Devlin Duldulao <webmasterdevlin@gmail.com>
2024-02-24 16:17:23 +0100

    Update _layout.tsx component

--- src/routes/_layout.tsx
+++ src/routes/_layout.tsx
@@ -1,14 +1,13 @@
+import { Outlet, createFileRoute } from '@tanstack/react-router';
 
-import { Outlet, createFileRoute } from "@tanstack/react-router";
-
-export const Route = createFileRoute("/_layout")({
+export const Route = createFileRoute('/_layout')({
   component: LayoutComponent,
 });
 
 function LayoutComponent() {
   return (
     <div>
-      <div>Layout</div>
+      <div>Layout (pathless)</div>
       <hr />
       <Outlet />
     </div>
