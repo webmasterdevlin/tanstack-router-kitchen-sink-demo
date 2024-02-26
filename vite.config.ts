0e2c581ff78acbb553a84a866a915cce8ebb8032
Devlin Duldulao <webmasterdevlin@gmail.com>
2024-02-26 11:11:30 +0100

    Add @tanstack/react-query and @tanstack/react-query-devtools

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import eslint from 'vite-plugin-eslint';
+import viteTsconfigPaths from 'vite-tsconfig-paths';
 import { TanStackRouterVite } from '@tanstack/router-vite-plugin';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), eslint(), TanStackRouterVite()],
+  plugins: [react(), eslint(), viteTsconfigPaths(), TanStackRouterVite()],
 });
