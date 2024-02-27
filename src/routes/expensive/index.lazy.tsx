1ebd3beafdc435ee0e26f87c6c6bdfc33241b2f6
Devlin Duldulao <webmasterdevlin@gmail.com>
2024-02-27 09:13:55 +0100

    Add error and pending components to Expensive route

--- src/routes/expensive/index.lazy.tsx
+++ src/routes/expensive/index.lazy.tsx
@@ -3,4 +3,10 @@ import Expensive from './-components/Expensive';
 
 export const Route = createLazyFileRoute('/expensive/')({
   component: Expensive,
+  errorComponent: () => {
+    return <div>Failed to load</div>;
+  },
+  pendingComponent: () => {
+    return <div>Loading...</div>;
+  },
 });
