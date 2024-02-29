4f4f0c492dc5faa1641903bf058a51e4b568c368
Devlin Duldulao <webmasterdevlin@gmail.com>
2024-02-29 15:49:16 +0100

    Add form validation and blocker to /contactus route

--- src/routes/contactus.tsx
+++ src/routes/contactus.tsx
@@ -1,7 +1,27 @@
-import { createFileRoute } from '@tanstack/react-router';
+import { Block, createFileRoute } from '@tanstack/react-router';
+import { useState } from 'react';
 
 export const Route = createFileRoute('/contactus')({
   component: () => {
-    return <div>Hello /contactus!</div>;
+    const [formIsDirty, setFromIsDirty] = useState(false);
+    return (
+      <Block
+        blocker={() => {
+          return window.confirm('Are you sure you want to leave?');
+        }}
+        condition={formIsDirty}
+      >
+        <div>Hello /contactus with url masking and blocker!</div>
+        <button
+          onClick={() => {
+            setFromIsDirty(prev => {
+              return !prev;
+            });
+          }}
+        >
+          {formIsDirty ? 'Form is dirty' : 'Form is clean'}
+        </button>
+      </Block>
+    );
   },
 });
