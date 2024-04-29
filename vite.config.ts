911d354661a66812ab4345e3c1fc2ad49e9659c2
Devlin Duldulao <webmasterdevlin@gmail.com>
2024-04-29 13:21:54 +0200

    Update dependencies in package.json and vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import eslint from 'vite-plugin-eslint';
 import viteTsconfigPaths from 'vite-tsconfig-paths';
 import { TanStackRouterVite } from '@tanstack/router-vite-plugin';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), eslint(), viteTsconfigPaths(), TanStackRouterVite()],
+  plugins: [react(), viteTsconfigPaths(), TanStackRouterVite()],
 });
