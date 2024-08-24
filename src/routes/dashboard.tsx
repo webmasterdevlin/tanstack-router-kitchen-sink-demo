a885540f350fc551103ecb07f569cedee719fcc2
Devlin Duldulao <webmasterdevlin@gmail.com>
2024-08-24 19:04:03 +0200

    update

--- src/routes/dashboard.tsx
+++ src/routes/dashboard.tsx
@@ -14,16 +14,8 @@ export const Route = createFileRoute('/dashboard')({
       //   to: '/',
       // });
     }
-    // Pass the devlin function to the route context. It creates or replaces a function.
-    return {
-      devlin: () => {
-        return console.log('devlin');
-      },
-    };
   },
-  loader: ({ context }) => {
-    context.devlin();
-  },
+
   // eslint-disable-next-line sort-keys-fix/sort-keys-fix
   component: () => {
     const auth = useAuth();
