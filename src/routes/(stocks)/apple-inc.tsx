e07b6f96dce063ec0e0021be8f4f1d7920695e29
Devlin Duldulao <webmasterdevlin@gmail.com>
2024-09-17 20:16:56 +0200

    remove old eslintrc

--- src/routes/(stocks)/apple-inc.tsx
+++ src/routes/(stocks)/apple-inc.tsx
@@ -1,5 +1,8 @@
 import { createFileRoute } from '@tanstack/react-router';
 
+/**
+ * Pathless Route Group Directories use `()` as a way to group routes files together regardless of their path. They are purely organizational and do not affect the route tree or component tree in any way.
+ */
 export const Route = createFileRoute('/(stocks)/apple-inc')({
   component: () => {
     return <div>Hello /(stocks)/apple-inc!</div>;
