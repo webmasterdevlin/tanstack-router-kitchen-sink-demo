e1de21ca4207742ce2032fd030682a054d19342f
Devlin Duldulao <webmasterdevlin@gmail.com>
2024-09-17 10:56:08 +0200

    update

--- src/routes/deferred-data.tsx
+++ src/routes/deferred-data.tsx
@@ -2,43 +2,42 @@ import { Await, createFileRoute, defer, useAwaited } from '@tanstack/react-route
 import axios from 'axios';
 
 export const Route = createFileRoute('/deferred-data')({
+    component: DeferComponent,
     loader: async () => {
-
         // comments and photos are deferred because they are not needed immediately
-        const deferredComments = defer(axios.get("https://jsonplaceholder.typicode.com/comments"))
-        const deferredPhotos = defer(axios.get("https://jsonplaceholder.typicode.com/photos"))
+        const deferredComments = defer(axios.get('https://jsonplaceholder.typicode.com/comments'));
+        const deferredPhotos = defer(axios.get('https://jsonplaceholder.typicode.com/photos'));
 
-        const users = await (axios.get("https://jsonplaceholder.typicode.com/users"))
+        const users = await axios.get('https://jsonplaceholder.typicode.com/users');
 
-        return { deferredComments, deferredPhotos, users }
+        return { deferredComments, deferredPhotos, users };
     },
-    component: DeferComponent
 });
 
-
 function DeferComponent() {
-    const { deferredComments, deferredPhotos, users } = Route.useLoaderData<LoaderDataType>()
+    const { deferredComments, deferredPhotos, users } = Route.useLoaderData<LoaderDataType>();
 
     // useAwaited and Await are used to handle the deferred promises but returns different data types
     const resultsPhotos = useAwaited({ promise: deferredPhotos });
 
     return (
-        <div className='container p-4'>
+        <div className="container p-4">
             <h1>Defer Page</h1>
             <p>Users: {JSON.stringify(users?.data).length}</p>
 
             <p>Photos (deferred): {JSON.stringify(resultsPhotos[0].data.length)}</p>
 
             <Await promise={deferredComments}>
-                {(resultsComments) => <p>Comments (deferred): {JSON.stringify(resultsComments.data.length)}</p>}
+                {resultsComments => {
+                    return <p>Comments (deferred): {JSON.stringify(resultsComments.data.length)}</p>;
+                }}
             </Await>
         </div>
-    )
+    );
 }
 
-
 interface LoaderDataType {
-    users: { data: any[] },
-    deferredPhotos: Promise<{ data: any[] }>,
-    deferredComments: Promise<{ data: any[] }>,
+    users: { data: any[] };
+    deferredPhotos: Promise<{ data: any[] }>;
+    deferredComments: Promise<{ data: any[] }>;
 }
